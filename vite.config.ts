fcf6a0445a2bc54609654b74411466ce0d7884c7
Lenni009 <guentherhanslphdyt@gmail.com>
2024-02-20 20:41:26 +0100

    avoid using __dirname

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 // vite.config.js
-import path, { resolve } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import { configDefaults } from 'vitest/config';
 import dts from 'vite-plugin-dts';
@@ -20,13 +20,13 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: fileURLToPath(new URL('src/index.ts', import.meta.url)),
       formats: ['es'],
     },
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   ...testDef,
