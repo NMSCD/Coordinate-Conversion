36689316aed3e19c4f4c6f2605cf65f91cefa8b3
Lenni009 <guentherhanslphdyt@gmail.com>
2024-02-20 21:01:01 +0100

    ignore vite config in build

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   base: './',
   plugins: [
     dts({
-      exclude: ['**/**.spec.**'],
+      exclude: ['**/**.spec.**', '*.config.*'],
       insertTypesEntry: true,
     }),
   ],
